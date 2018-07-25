53bf33b28bb66290006bbf227f7a3aeecf3bb1d9
Google Google <Google@students.softuni.bg>
2018-07-25 16:40:24 +0300

    Changes

---
+++ 18.AutomappingObjects/Google/Google.App/Core/CommandInterpreter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Google.App.Core.Contracts;
+
+namespace Google.App.Core
+{
+  public  class CommandInterpreter:ICommandInterpreter
+  {
+      private readonly IServiceProvider serviceProvider;
+
+        public CommandInterpreter(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public string Read(string[] input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
