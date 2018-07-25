d82b8561f5060c61fa274081d81d87385725ff92
Google Google <Google@students.softuni.bg>
2018-07-25 16:25:09 +0300

    Google Automapping

---
+++ 18.AutomappingObjects/Google/Google.App/Core/Engine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Google.App.Core.Contracts;
+
+namespace Google.App.Core
+{
+    public class Engine : IEngine
+    {
+        public void Run()
+        {
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
---
+++ 18.AutomappingObjects/Google/Google.App/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Google.Data;
+using Google.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Google.App
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var service = ConfigureService();
+        }
+
+        private static IServiceProvider ConfigureService()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddDbContext<GoogleContext>(op => op.UseSqlServer(Configuration.ConnectionString));
+
+            serviceCollection.AddTransient<IDbInitializerService, DbInitializerService>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            return serviceProvider;
+        }
+    }
+}
