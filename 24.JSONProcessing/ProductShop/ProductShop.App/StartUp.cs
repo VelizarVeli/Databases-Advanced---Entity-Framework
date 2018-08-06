2ab3b64fe203b276cf1742c0aa302c91dd4c1095
Google Google <Google@students.softuni.bg>
2018-08-06 09:50:17 +0300

    JSONProcessing

---
+++ 24.JSONProcessing/ProductShop/ProductShop.App/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ProductShop.App
+{
+    using AutoMapper;
+
+    using Data;
+    using Models;
+    using Newtonsoft.Json;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+            var mapper = config.CreateMapper();
+
+            var context = new ProductShopContext();
+
+            var jsonString = File.ReadAllText("Json/users.json");
+
+            var deserializedUsers = JsonConvert.DeserializeObject<User[]>(jsonString);
+
+            foreach (var user in deserializedUsers)
+            {
+                Console.WriteLine(user.FirstName);
+            }
+
+        }
+    }
+}
