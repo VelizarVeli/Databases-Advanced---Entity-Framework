0a62b9fc4e1d0b9ae4ad63581c51fa8c2bfebe8c
Google Google <Google@students.softuni.bg>
2018-10-28 09:43:08 +0200

    456

--- ExamPreps/AuthorFastFood/FastFood.Models/OrderItem.cs
+++ ExamPreps/AuthorFastFood/FastFood.Models/OrderItem.cs
@@ -5,12 +5,10 @@ namespace FastFood.Models
 	public class OrderItem
 	{
 		public int OrderId { get; set; }
-
 		[Required]
 		public Order Order { get; set; }
 
 		public int ItemId { get; set; }
-
 		[Required]
 		public Item Item { get; set; }
 
